387ad39797f028c8822b6acc04d5c3bce1945561
kyosukenakashima <regolith.mail@gmail.com>
2019-04-26 02:01:20 +0900

    JSXでのJavaScript表記

--- test/src/index.js
+++ test/src/index.js
@@ -15,14 +15,17 @@ for (let i = 0; i < 12; i++) {
 
 function Hello() {
   return(
-    <div>
+ <section>
       <h1>
        こんにちは { name.lastName + name.firstName} さん　{ count + 1 } 回目の訪問です
         </h1>
-      <p>
+      <p id="hello">
         Helloコンポーネントが書き出されました！
       </p>
-    </div>
+
+      <div style={{'margin-top': '100px', 'padding': '50px'}}>
+      <img src="https://goo.gl/0kZocc" alt="Google" /></div>
+    </section>
   );
 }
 
