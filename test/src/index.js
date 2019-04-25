ee5b8c7b23b474eb98387fbe22c5342138996249
kyosukenakashima <regolith.mail@gmail.com>
2019-04-26 01:37:53 +0900

    JSX内でのJavaScript表記

--- test/src/index.js
+++ test/src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+const name = {
+  firstName: '杏介',
+  lastName: '中島'
+};
+
+let count = 0;
+for (let i = 0; i < 12; i++) {
+  count ++;
+} 
+
 function Hello() {
   return(
     <div>
       <h1>
-       こんにちは
+       こんにちは { name.lastName + name.firstName} さん　{ count + 1 } 回目の訪問です
         </h1>
       <p>
         Helloコンポーネントが書き出されました！
